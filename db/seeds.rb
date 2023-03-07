fece42429f316c6fa8084744f0bc5cadae38aa0a
Meredith Trevino <meredithtrevino3@gmail.com>
2023-03-07 10:00:02 -0700

    Adds seed data to test with rails s

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-garden = Garden.create!(name: 'Turing Community Garden', organic: true)
-  
-plot1 = garden.plots.create!(number: 1, size: 'Large', direction: 'East')
-plot2 = garden.plots.create!(number: 2, size: 'Medium', direction: 'West')
-plot3 = garden.plots.create!(number: 3, size: 'Small', direction: 'North')
+garden1 = Garden.create!(name: 'Turing Community Garden', organic: true)
+garden2 = Garden.create!(name: 'Baker Community Garden', organic: false)
+
+plot1 = garden1.plots.create!(number: 1, size: 'Large', direction: 'East')
+plot2 = garden1.plots.create!(number: 2, size: 'Medium', direction: 'West')
+plot3 = garden1.plots.create!(number: 3, size: 'Small', direction: 'North')
+plot4 = garden2.plots.create!(number: 4, size: 'Small', direction: 'South')
 
 plant1 = Plant.create!(name: 'Lavender', description: 'Herb', days_to_harvest: 90)
 plant2 = Plant.create!(name: 'Rosemary', description: 'Herb', days_to_harvest: 100)
@@ -24,3 +26,5 @@ plant3 = Plant.create!(name: 'Tomato', description: 'Vegetable', days_to_harvest
   PlantPlot.create!(plant: plant2, plot: plot2)
 
   PlantPlot.create!(plant: plant3, plot: plot3)
+  
+  PlantPlot.create!(plant: plant1, plot: plot4)
